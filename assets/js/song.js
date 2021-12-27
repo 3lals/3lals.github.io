6e785f314aeda4f9f8c01e26bd4f78fd57f088b8
3lals <96339574+3lals@users.noreply.github.com>
2021-12-27 20:27:02 +0700

    Add files via upload

---
+++ assets/js/song.js
@@ -0,0 +1,84 @@
+let songs = [
+  {
+    "id": 1,
+    "title": "Missing You Today",
+    "artist": "Davichi",
+    "bpm": 122,
+    "video_id": "iA9olaeQBvI",
+    "img_thumb": "https://s.mxmcdn.net/images-storage/albums4/6/1/2/7/8/1/40187216_500_500.jpg"
+  },
+  {
+    "id": 2,
+    "title": "You Are My Everything",
+    "artist": "B-ROCK, TIMBER",
+    "bpm": 158,
+    "video_id": "SuFl3RaPO9o",
+    "img_thumb": "https://s.mxmcdn.net/images-storage/albums4/1/2/6/9/4/7/32749621_500_500.jpg"
+  },
+  {
+    "id": 3,
+    "title": "Living In The Heart",
+    "artist": "Untouchable Ft. Narsha",
+    "bpm": 144,
+    "video_id": "GxDeW0Af9DI",
+    "img_thumb": "https://s.mxmcdn.net/images-storage/albums5/0/7/5/3/1/6/38613570_350_350.jpg"
+  },
+  {
+    "id": 4,
+    "title": "Can You Hear Your Heart",
+    "artist": "EPIK HIGH Ft. Lee Hi",
+    "bpm": 190,
+    "video_id": "F_9pZv0wn9Q",
+    "img_thumb": "https://s.mxmcdn.net/images-storage/albums4/8/8/8/3/5/1/35153888_500_500.jpg"
+  },
+  {
+    "id": 5,
+    "title": "SELAMAT (Selamat Tinggal)",
+    "artist": "Virgoun Ft. Audi",
+    "bpm": 80,
+    "video_id": "nnZpgbJQVXw",
+    "img_thumb": "https://s.mxmcdn.net/images-storage/albums4/7/3/2/6/5/1/42156237_500_500.jpg"
+  },
+  {
+    "title": "Can't Believe It",
+    "artist": "Younha",
+    "bpm": 93,
+    "video_id": "vc7sbQeLE78",
+    "img_thumb": "https://s.mxmcdn.net/images-storage/albums4/1/0/8/3/0/5/41503801_500_500.jpg"
+  },
+  {
+    "title": "Song For You",
+    "artist": "Ahn Young Min, So Yeon",
+    "bpm": 158,
+    "video_id": "ab1_PnmIaRg",
+    "img_thumb": "https://s.mxmcdn.net/images-storage/albums/4/2/9/0/1/8/30810924_500_500.jpg"
+  },
+  {
+    "title": "Pretender",
+    "artist": "DISM",
+    "bpm": 92,
+    "video_id": "TQ8WlA2GXbk",
+    "img_thumb": "https://s.mxmcdn.net/images-storage/albums4/0/8/7/9/5/2/43259780_500_500.jpg"
+  },
+  {
+    "title": "Easy On Me",
+    "artist": "Adele",
+    "bpm": 144,
+    "video_id": "X-yIEMduRXk",
+    "img_thumb": "https://s.mxmcdn.net/images-storage/albums2/0/3/3/1/3/2/59231330_500_500.jpg"
+  },
+  {
+    "title": "Loving You",
+    "artist": "So Eun Lee",
+    "bpm": 72,
+    "video_id": "JJjaEgsFTQE",
+    "img_thumb": "https://s.mxmcdn.net/images-storage/albums5/9/5/0/6/9/1/33196059_500_500.jpg"
+  },
+  {
+    "title": "Today More Than Yesterday",
+    "artist": "Kim Jong Kook",
+    "bpm": 83,
+    "video_id": "SqQtqUPNH_M",
+    "img_thumb": "https://s.mxmcdn.net/images-storage/albums/6/2/1/5/7/2/14275126_500_500.jpg"
+  }
+]
